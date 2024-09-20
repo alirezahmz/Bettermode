c23128686ec5c018f354ed08707aca96cd95c81a
alirezahmz <alireza.hamzeh7713@gmail.com>
2024-09-20 09:20:54 +0330

    refactor: 🎨 Extract expandable section into a separate component

--- src/components/containers/post/expandableContent/ExpandableContent.stories.tsx
+++ src/components/containers/post/expandableContent/ExpandableContent.stories.tsx
@@ -1,22 +1,20 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+// ExpandableContent.stories.tsx
 import { Meta, StoryObj } from "@storybook/react";
-import Card from "./Card";
+import ExpandableContent from "./ExpandableContent";
+import { IExpandableContentProps } from "./types";
 
-const meta: Meta<typeof Card> = {
-  component: Card,
-  title: "Components/containers/Card",
+const meta: Meta<typeof ExpandableContent> = {
+  component: ExpandableContent,
+  title: "Components/containers/ExpandableContent",
   parameters: {
-    layout: "centered",
-    nextjs: {
-      appDirectory: true
-    }
+    layout: "centered"
   }
 };
 export default meta;
 
-type Story = StoryObj<typeof Card>;
+type Story = StoryObj<typeof ExpandableContent>;
 
-export const Example: Story = {
+export const Default: Story = {
   args: {
     fields: [
       { key: "1", value: "<p>This is the 1 content block.</p>" },
@@ -30,14 +28,7 @@ export const Example: Story = {
       { key: "9", value: "<p>This is the 9 content block.</p>" },
       { key: "10", value: "<p>This is the 10 content block.</p>" },
       { key: "11", value: "<p>This is the 11 content block.</p>" },
-      { key: "12", value: "<p>This is the 12 content block.</p>" },
-      { key: "13", value: "<p>This is the 13 content block.</p>" },
-      { key: "14", value: "<p>This is the 14 content block.</p>" },
-      { key: "15", value: "<p>This is the 15 content block.</p>" }
-    ],
-    title: "This is a sample card title",
-    reactionsCount: 5,
-    createdAt: new Date().toISOString(),
-    onAddReaction: () => alert("Reaction added!")
-  }
+      { key: "12", value: "<p>This is the 12 content block.</p>" }
+    ]
+  } as IExpandableContentProps
 };
